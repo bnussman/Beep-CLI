7b5d120afb5889f57e34453b27b05f8cb1ff2c3d
Banks Nussman <banks@nussman.us>
2021-01-23 10:48:08 -0500

    quick way to clear a beepers queue and reset riders data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Location from './location';
+import Queues from './queues';
 import database from './utils/db';
 
 const input: string[] = process.argv.slice(2);
@@ -8,6 +9,13 @@ function main() {
         if (input[1] == "migrate") {
             const l = new Location();
             l.migrate();
+        }
+    }
+
+    if (input[0] == "queue") {
+        if (input[1] == "clear") {
+            const q = new Queues();
+            q.clear(input[2]);
         }
     }
 }
